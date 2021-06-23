35588a77fe5b07f979af5ccbb80c65398869247e
erwan benard <erwan.benard@epita.fr>
2021-06-23 15:05:04 +0200

    First API Fix 404 error case

--- src/main/java/fr/lernejo/navy_battle/APOD.java
+++ src/main/java/fr/lernejo/navy_battle/APOD.java
@@ -4,16 +4,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 
 public class APOD {
-    public final JsonNode id;
-    public final JsonNode url;
-    public final JsonNode message;
+    public final String id;
+    public final String url;
+    public final String message;
 
     public APOD(@JsonProperty("id") JsonNode id,
                 @JsonProperty("url") JsonNode url,
                 @JsonProperty("message") JsonNode message) {
-        this.id = id;
-        this.url = url;
-        this.message = message;
+        this.id = id.toString();
+        this.url = url.toString();
+        this.message = message.toString();
     }
 
 }
--- src/main/java/fr/lernejo/navy_battle/CustomStartHandler.java
+++ src/main/java/fr/lernejo/navy_battle/CustomStartHandler.java
@@ -1,87 +1,79 @@
 package fr.lernejo.navy_battle;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 public class CustomStartHandler implements HttpHandler {
 
     private final StringBuilder url = new StringBuilder();
 
-    CustomStartHandler(int port) {
-        this.url.append("http://localhost:").append(Integer.toString(port));
-    }
+    CustomStartHandler(int port) { this.url.append("http://localhost:").append(port); }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
         if (!exchange.getRequestMethod().equals("POST")) {
-            System.out.println("Get method received");
-            throw new IOException();
+            NotFoundMethod(exchange);
         }
+        else {
+            APOD requestJson = ParseBody(exchange);
 
-        var s = exchange.getRequestBody();
-        ObjectMapper mapper = new ObjectMapper();
-        APOD requestJson = null;
-        String Body = "Bad request";
+            if (requestJson.message.equals("\"\"") || requestJson.id.equals("\"\"") || requestJson.url.equals("\"\"")) {
+                exchange.sendResponseHeaders(400, "Bad Json".length());
+                try (OutputStream os = exchange.getResponseBody()) { // (1)
+                    os.write("Bad Json".getBytes());
+                }
+            }
+            else {
+                StringBuilder test = new StringBuilder();
+                String msg = "May the best code win";
+                test.append("{\n\t\"id\":\"000\",\n\t\"url\":\"").append(this.url).append("\",\n\t\"message\":\"").append(msg).append("\"\n}");
 
+                exchange.sendResponseHeaders(202, test.toString().length());
+                try (OutputStream os = exchange.getResponseBody()) { // (1)
+                    os.write(test.toString().getBytes());
+                }
+            }
+        }
+    }
+
+    private void NotFoundMethod(HttpExchange exchange) throws IOException {
+        String error = "TNull";
+        exchange.sendResponseHeaders(404, error.length());
+        try (OutputStream os = exchange.getResponseBody()) { // (1)
+            os.write(error.getBytes());
+        }
+    }
+
+    private String ConvertInputStreamtoString(InputStream s) throws IOException {
         StringBuilder sb = new StringBuilder();
         int c;
         while ((c = s.read()) > 0) {
             sb.append((char) c);
         }
+        return sb.toString();
+    }
+
+    private APOD ParseBody(HttpExchange exchange) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        APOD requestJson = null;
+        String Body = "Bad request";
+
+        String sb = ConvertInputStreamtoString(exchange.getRequestBody());
 
         try {
-            requestJson = mapper.readValue(sb.toString(), APOD.class);
+            requestJson = mapper.readValue(sb, APOD.class);
         } catch (IllegalArgumentException e) {
-            System.err.println("Wrong Json");
             exchange.sendResponseHeaders(400, Body.length());
             throw new IllegalArgumentException();
         }
 
-        StringBuilder test = new StringBuilder();
-        String msg = "May the best code win";
-        test.append("{\n\t\"id\":\"000\",\n\t\"url\":\"")
-            .append(this.url).append("\",\n\t\"message\":\"")
-            .append(msg)
-            .append("\"\n}");
-
-        exchange.sendResponseHeaders(202, /*sb*/test.toString().length());
-        try (OutputStream os = exchange.getResponseBody()) { // (1)
-            os.write(/*sb*/test.toString().getBytes());
-        }
-
-        /*JsonNode rb = mapper.readTree(sb.toString());
-        APOD requestJson = new APOD(rb.get("id"), rb.get("url"), rb.get("message"));
-
-        StringBuilder code = new StringBuilder();
-
-        int rcode = 202;
-        if (requestJson.id.isNull()) {
-            System.err.println("no id in the request");
-            code.append("Bad Request");
-            rcode = 404;
-        }
-        else if (requestJson.url.isNull()) {
-            System.err.println("no url in the request");
-            code.append("Bad Request");
-            rcode = 404;
-        }
-        else if (requestJson.message.isNull()) {
-            System.err.println("no message in the request");
-            code.append("Bad Request");
-            rcode = 404;
-        }
-        else
-            code.append("Accepted");
-
-        exchange.sendResponseHeaders(rcode, code.length());
-        try (OutputStream os = exchange.getResponseBody()) { // (1)
-            os.write(code.toString().getBytes());
-        }*/
-
+        return requestJson;
     }
 }
