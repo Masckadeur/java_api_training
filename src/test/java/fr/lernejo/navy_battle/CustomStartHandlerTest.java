3a9489290ee281ea18f0e01e19b7e9834efe83b8
erwan benard <erwan.benard@epita.fr>
2021-06-23 15:06:33 +0200

    Add test for CustomStartHandler

---
+++ src/test/java/fr/lernejo/navy_battle/CustomStartHandlerTest.java
@@ -0,0 +1,45 @@
+package fr.lernejo.navy_battle;
+
+import com.sun.net.httpserver.HttpServer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomStartHandlerTest {
+
+    @Test
+    void StartHandlerGetMethosTest() throws IOException, InterruptedException {
+        new StartServer().Start(9876);
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:9876/api/game/start"))
+            .GET()
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertEquals(404, response.statusCode());
+    }
+
+    @Test
+    void StartHandlerDELETEMethosTest() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:9876/api/game/start"))
+            .DELETE()
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertEquals(404, response.statusCode());
+    }
+}
