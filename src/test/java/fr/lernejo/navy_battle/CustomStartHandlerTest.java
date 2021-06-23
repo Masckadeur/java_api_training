291776555518b908484a257ec26b79c9bf76b517
erwan benard <erwan.benard@epita.fr>
2021-06-23 19:23:37 +0200

    Add tests for API Game Start

--- src/test/java/fr/lernejo/navy_battle/CustomStartHandlerTest.java
+++ src/test/java/fr/lernejo/navy_battle/CustomStartHandlerTest.java
@@ -45,4 +45,84 @@ class CustomStartHandlerTest {
         Assertions.assertEquals(404, response.statusCode());
         server.Stop();
     }
+
+    @Test
+    void StartHandlerPUTMethosTest() throws IOException, InterruptedException {
+        var server = new StartServer(9876);
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:9876/api/game/start"))
+            .PUT(HttpRequest.BodyPublishers.noBody())
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertEquals(404, response.statusCode());
+        server.Stop();
+    }
+
+    @Test
+    void StartHandlePost() throws IOException, InterruptedException {
+        var server = new StartServer(9876);
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:9876/api/game/start"))
+            .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"1\", \"url\":\"http://localhost:" + 9876 + "\", \"message\":\"I will crush you!\"}"))
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertEquals(202, response.statusCode());
+        server.Stop();
+    }
+
+    @Test
+    void StartHandlePostNoUrl() throws IOException, InterruptedException {
+        var server = new StartServer(9876);
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:9876/api/game/start"))
+            .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"1\", \"url\":\"" + "\", \"message\":\"I will crush you!\"}"))
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertEquals(400, response.statusCode());
+        server.Stop();
+    }
+
+    @Test
+    void StartHandlePostNoId() throws IOException, InterruptedException {
+        var server = new StartServer(9876);
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:9876/api/game/start"))
+            .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"\", \"url\":\"http://localhost:" + 9876 + "\", \"message\":\"I will crush you!\"}"))
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertEquals(400, response.statusCode());
+        server.Stop();
+    }
+
+    @Test
+    void StartHandlePostNoMessage() throws IOException, InterruptedException {
+        var server = new StartServer(9876);
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create("http://localhost:9876/api/game/start"))
+            .POST(HttpRequest.BodyPublishers.ofString("{\"id\":\"1\", \"url\":\"http://localhost:" + 9876 + "\", \"message\":\"\"}"))
+            .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        Assertions.assertEquals(400, response.statusCode());
+        server.Stop();
+    }
 }
